9f5aff2baaf89d9d23e1cb6884545911ead1c3cd
CodeTanish <rastogitanish673@gmail.com>
2025-04-30 22:59:35 +0530

    use of Binary Search Technique

---
+++ Arrays/binarySearch/Search.java
@@ -0,0 +1,22 @@
+public class Search {
+    public static void main(String [] arg){
+        int [] arr = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+
+        searchArray(arr, target);
+    }
+
+    static int searchArray(int [] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        int mid = 0;
+        
+        while(start < end){
+            mid = (start + end)/2;
+
+            if(arr[mid] > target) end = mid -1;
+            else start = mid + 1;
+        }
+        return (arr[mid] == target) ? mid : -1;
+    }
+}
